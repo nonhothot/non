7911f938093f049a4ca035f0071c87c54e0d08a0
nonhothot <nonhothot@gmail.com>
2020-10-01 11:05:03 +0700

    scam

---
+++ brave tech.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>Brave tech company</p>
+<p>this company will drive with Digital curren cy cause This is Future</p>
+<p>AI will make your life easier than now</p>
+<p>you should make it happen</p>
+<p>Fighting Brave Group</p>
+<p>&nbsp;</p>
+</body>
+</html>
